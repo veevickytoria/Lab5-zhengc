a63260d1f2b3517e9f4b47d4f7e8b67a15fe169b
zhengc <zhengc@rose-hulman.edu>
2012-03-22 15:04:23 -0400

    testOne written

--- Prime Factorization/src/PrimeFactorization.java
+++ Prime Factorization/src/PrimeFactorization.java
@@ -1,12 +1,7 @@
+import java.util.ArrayList;
 
 public class PrimeFactorization {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static ArrayList<Integer> primeFactors(int number){
+		return null;
 	}
-
 }
--- Prime Factorization/src/PrimeFactoriztionTest.java
+++ Prime Factorization/src/PrimeFactoriztionTest.java
@@ -1,5 +1,9 @@
-
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Test;
 
 public class PrimeFactoriztionTest {
-
+	public void testOne(){
+		assertEquals(new ArrayList<Integer>(),PrimeFactorization.primeFactors(1));
+	}
 }
