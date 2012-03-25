081e413d33b4af14bef88da245a8bf3286055b7c
zhengc <zhengc@rose-hulman.edu>
2012-03-25 14:56:06 -0400

    test two and three passed

--- Prime Factorization/src/PrimeNumberGenerator.java
+++ Prime Factorization/src/PrimeNumberGenerator.java
@@ -3,11 +3,11 @@ import java.util.ArrayList;
 
 public class PrimeNumberGenerator {
 	public static ArrayList<Integer> generatePrimes(int n){
-		if(n < 2)
+		if(n < 3)
 			return new ArrayList<Integer>();
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		for(int i = 2; i < n; i++){
-			ret.add(n);
+			ret.add(i);
 		}
 		return ret;
 	}
