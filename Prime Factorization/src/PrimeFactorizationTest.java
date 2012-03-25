01f5bbc8abd401d99fc2a15bbe5342df72e3ccde
zhengc <zhengc@rose-hulman.edu>
2012-03-25 13:50:00 -0400

    second test written

--- Prime Factorization/src/PrimeFactorizationTest.java
+++ Prime Factorization/src/PrimeFactorizationTest.java
@@ -9,4 +9,9 @@ public class PrimeFactorizationTest {
 	public void testOne(){
 		assertEquals(new ArrayList<Integer>(),PrimeFactorization.primeFactors(1));
 	}
+	
+	@Test
+	public void testTwo(){
+		
+	}
 }
