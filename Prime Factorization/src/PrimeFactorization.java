d375ffeb6039788e60d8b1c3e032b4e3d4cb837a
zhengc <zhengc@rose-hulman.edu>
2012-03-25 14:11:38 -0400

    second test passed

--- Prime Factorization/src/PrimeFactorization.java
+++ Prime Factorization/src/PrimeFactorization.java
@@ -5,7 +5,17 @@ public class PrimeFactorization {
 		if (number == 1){
 			return new ArrayList<Integer>();
 		}else{
-			return null;
+			int n = number; 
+			ArrayList<Integer> factors = new ArrayList<Integer>();
+			
+			for (int i = 2; i <= n; i++) {
+				while (n % i == 0) {
+					factors.add(i);
+					n /= i;
+				}
+			}
+			
+			return factors;
 		}
 	}
 }
