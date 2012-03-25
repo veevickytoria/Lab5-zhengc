0cf3db2f82cac6b4be21eec252ea9a061ae92cfb
zhengc <zhengc@rose-hulman.edu>
2012-03-25 14:01:46 -0400

    fixed typo, second test passed

--- Prime Factorization/src/PrimeFactoriztionTest.java
+++
@@ -1,9 +0,0 @@
-import static org.junit.Assert.*;
-import java.util.ArrayList;
-import org.junit.Test;
-
-public class PrimeFactoriztionTest {
-	public void testOne(){
-		assertEquals(new ArrayList<Integer>(),PrimeFactorization.primeFactors(1));
-	}
-}
